208fc6ea97dac13db6e791b8069bd6d93087a9fe
olearybmps <160647100+olearybmps@users.noreply.github.com>
2024-03-15 13:04:44 -0400

    commit after problem 3

--- app.js
+++ app.js
@@ -38,19 +38,63 @@
 // Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. 
 // Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
 // Be careful! If you set n to a number too large, your loop could take a long time to process.
-let num = 7;
-while (true) {
-    let isPrime = true;
-    let nextNum = num + 1;
-    for (let i = 2; i < nextNum; i++) {
-        if (nextNum % i === 0) {
-            isPrime = false;
-            break;
-        }
+// let num = 7;
+// while (true) {
+//     let isPrime = true;
+//     let nextNum = num + 1;
+//     for (let i = 2; i < nextNum; i++) {
+//         if (nextNum % i === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(nextNum);
+//         break;
+//     }
+//     num = nextNum;
+// }
+// Part 3: Feeling Loopy
+// As a final task, solve the following practical problem regarding string processing.
+// Your task is to write a script that accomplishes the following:
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+// You can make the following assumptions:
+// There will only be 4 cells per row.
+// There will be no escaped characters other than “\n”.
+// Use the example string provided above to test your program. 
+// Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
+// Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138
+// \n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
+let strCSV = ("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26")
+let cell = "";
+let row = [];
+let dataCells = [];
+
+for (i = 0; i < strCSV.length; i++) {
+    const char = strCSV[i];
+
+    if(char === ","){
+        row.push(cell);
+        cell = "";
+    } else if (char === "\n" || char === "\r") {
+        dataCells.push(cell);
+        console.log(row.join(" "));
+        dataCells.push(row);
+        row = [];
+        cell = "";
+    }else{
+        cell += char;
     }
-    if (isPrime) {
-        console.log(nextNum);
-        break;
-    }
-    num = nextNum;
 }
+if (row.length > 0) {
+    console.log(row.join(" "));
+    dataCells.push(row);
+}
+
+console.log(dataCells);
